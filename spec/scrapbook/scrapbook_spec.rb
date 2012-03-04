8327e1a52406611cc4c4db84cafa2e4c05142b81
ContrarySheep <contrarysheep@gmail.com>
2012-03-04 12:05:24 -0500

    Added tests

---
+++ spec/scrapbook/scrapbook_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe Scrapbook do
+
+	pending "should generate the scrapbook intializer file"
+
+	pending "should generate the scrapbook cache file"
+
+	pending "should evaluate the scrapbook cache"
+
+	pending "should refresh the scrapbook"
+
+end
